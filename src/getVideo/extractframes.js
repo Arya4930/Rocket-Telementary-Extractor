8392b40bddc7a806adc07663643097f1684b6d7f
Arya <aryapanwar493@gmail.com>
2025-03-30 12:02:47 +0530

    Adding cli loading bar

--- src/getVideo/extractframes.js
+++ src/getVideo/extractframes.js
@@ -1,13 +1,20 @@
 import ffmpeg from 'fluent-ffmpeg';
+import { getVideoDurationInSeconds } from 'get-video-duration';
+import { updateProgressBar } from '../utils/Croplmages.js';
 
 export default async function extractFrames(videopath, directoryPath) {
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
+        let processed = 0;
+        let total = await getVideoDurationInSeconds(videopath);
         ffmpeg(videopath)
             .videoFilters('fps=1')
             .output(`${directoryPath}/frame_%05d.png`)
             .noAudio()
             .on('start', function (commandLine) {
                 console.log('Started: ' + commandLine);
+            })
+            .on('progress', function (progress) {
+                updateProgressBar(progress.frames, total, 'Extracting Frames');
             })
             .on('error', function (err) {
                 reject('An error occurred: ' + err.message);
